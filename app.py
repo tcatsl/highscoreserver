34b23341722b9d156600527b69d396fdcc42a2cb
tcatsl <thomas.castleman@gmail.com>
2017-04-27 13:27:14 -0600

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 import models
-secret = os.environ['secret']
+secret = os.environ['SECRET']
 @app.route('/', methods=['GET'])
 def scores():
     token = request.headers.get('secret')
