36fb5d265edb62563f45fbcd8baac28f4102c7e0
tcatsl <thomas.castleman@gmail.com>
2017-04-27 13:24:58 -0600

    test

--- models.py
+++ models.py
@@ -2,11 +2,11 @@ from app import db
 
 class Scores(db.Model):
     @property
-        def serialize(self):
-            return {
-                'score' : self.score,
-                'name': self.name
-            }
+    def serialize(self):
+        return {
+            'score' : self.score,
+            'name': self.name
+        }
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
