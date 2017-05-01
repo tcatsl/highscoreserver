3bdac4a4673f8401aa80b85ed5d1ed1e77a6abef
tcatsl <thomas.castleman@gmail.com>
2017-05-01 10:25:36 -0600

    est

--- app.py
+++ app.py
@@ -19,7 +19,30 @@ auth_key = os.environ['AUTH0']
 port = int(os.environ.get('PORT', 33507))
 CORS(app)
 
+def get_auth():
+    auth = request.headers.get('Authorization')
+    if not auth:
+        abort(404)
 
+    parts = auth.split()
+
+    if parts[0].lower() != 'bearer':
+        abort(404)
+    elif len(parts) == 1:
+        abort(404)
+    elif len(parts) > 2:
+        abort(404)
+    token = parts[1]
+    try:
+        return jwt.decode(
+            token,
+            auth_key,
+            audience='YsIZfSDkbOYRuIDFuH64NhRywmQpz1kJ'
+        )
+    except jwt.ExpiredSignature:
+        abort(404)
+    except jwt.DecodeError:
+        abort(404)
 
 @app.route('/latest', methods=['GET'])
 def latest():
@@ -31,29 +54,7 @@ def scores():
 
 @app.route('/', methods=['POST'])
 def post_scores():
-    auth = request.headers.get('Authorization')
-    if not auth:
-        abort(403)
-
-    parts = auth.split()
-
-    if parts[0].lower() != 'bearer':
-        abort(405)
-    elif len(parts) == 1:
-        abort(406)
-    elif len(parts) > 2:
-        abort(407)
-    token = parts[1]
-    try:
-        payload = jwt.decode(
-            token,
-            auth_key,
-            audience='YsIZfSDkbOYRuIDFuH64NhRywmQpz1kJ'
-        )
-    except jwt.ExpiredSignature:
-        abort(408)
-    except jwt.DecodeError:
-        abort(409)
+    the_payload = get_auth()
     score_obj = json.loads(request.data)
     db.session.add(models.Scores(score_obj['name'], score_obj['score']))
     db.session.commit()
