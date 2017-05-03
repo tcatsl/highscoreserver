0031a6611b7631a16c89b9eb1a9d1ac2bb01a5b6
tcatsl <thomas.castleman@gmail.com>
2017-05-03 11:15:58 -0600

    test

--- app.py
+++ app.py
@@ -57,14 +57,14 @@ def post_scores():
     the_payload = get_auth()
     print(the_payload)
     score_obj = json.loads(request.data)
-    db.session.add(models.Scores(users.App_users.query('user_name').filter(users.App_users.user_name == the_payload['email']))[0], score_obj['score'], score_obj['kills'], score_obj['difficulty'], score_obj['duration']))
+    db.session.add(models.Scores(users.App_users.query('user_name').filter(users.App_users.user_name == the_payload['email']))[0], score_obj['score'], score_obj['kills'], score_obj['difficulty'], score_obj['duration'])
     db.session.commit()
     return jsonify(data=[i.serialize for i in models.Scores.query.order_by('score desc').all()])
 
 @app.router('/is_user', methods=['GET'])
     payload = get_auth()
     if (len(users.App_users.query("user_name").filter(users.App_users.email == payload['email'])) > 0):
-        return users.App_users.query("user_name").filter(users.App_users.user_name == payload['email']))[0]
+        return users.App_users.query("user_name").filter(users.App_users.user_name == payload['email'])[0]
     else:
         return ""
 
